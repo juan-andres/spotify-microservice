4635fc6c911d266ac02597a71158b241daeb13cb
JuanAndres Andrango <andrango.juanandres@gmail.com>
2017-01-19 00:01:04 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,21 @@
+var http = require('http');
+var url = require('url');
+
+// Datastore lol
+var store = {
+	access_token: '',
+	refresh_token: '',
+	playlist: null,
+	last_requested: ''
+};
+
+http.createServer(function(req, res) {
+	if (req.url === '/playlistOfTheMonth') {
+		res.end('January2017');
+	} else if (req.url === '/health') {
+		res.end('Alive!');
+	} else {
+		res.writeHead(400);
+		res.end('Bad Request');
+	}
+}).listen(process.env.PORT || 8888);
